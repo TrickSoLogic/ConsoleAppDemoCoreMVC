4bdc3e7fd16c24a98977ac9cf9013eaf1935b773
Vishal <vishalbhat.com@gmail.com>
2023-05-05 12:27:32 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppDemoCoreMVC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Coder! Happy Coding, Black-Coder");
+        }
+    }
+}
